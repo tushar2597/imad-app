3372e3005a9390dfde6fcc220f925f9aa7eba1f5
tushar2597 <31357358+tushar2597@users.noreply.github.com>
2017-08-28 01:31:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function createmplate(data){
     var date=data.date;
     var heading=data.heading;
     var content=data.content;
-var htmltemplate -`
+var htmltemplate =`
 <html>
     <head>
         <title>
