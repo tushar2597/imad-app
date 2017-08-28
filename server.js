50855bef37c2495dfff45c4bc0bd9184f237966e
tushar2597 <31357358+tushar2597@users.noreply.github.com>
2017-08-28 15:08:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,10 @@ app.get('/article-three',function(req,res){
 }
 
 );
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.css'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
