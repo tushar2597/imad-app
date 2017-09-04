9013acd046ff28effa9fdb93f943d8001f5ee131
tushar2597 <31357358+tushar2597@users.noreply.github.com>
2017-09-04 23:39:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/ui/madi.png', function (req, res) {
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
-    res.send(counter,toString())
+    res.send(counter,toString());
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
