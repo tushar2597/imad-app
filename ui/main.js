e4606d00ba8cdd3bb2def0f71ff7b24a6a070a2f
tushar2597 <31357358+tushar2597@users.noreply.github.com>
2017-09-04 22:40:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,14 @@ console.log('Loaded!');
 var element= document.getElementById("maintext");// changing the tewxt using javascript.
 element.innerHTML="NEW VALUES SET USING JAVASCRIPT";
 
+
+
+
+//making the image move
+var t=document.getElementById('image');
+t.onclick=function(){
+    image.style.marginLeft='100px';
+}
 var img= document.getElementById('image');
 var marginleft=0;
 function moveright(){
