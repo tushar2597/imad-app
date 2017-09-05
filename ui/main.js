e2acd38f6668bc0725afee3035475a7f7bdaa3bf
tushar2597 <31357358+tushar2597@users.noreply.github.com>
2017-09-06 01:36:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,35 +1,75 @@
 console.log('Loaded!');
 
+var button =document.getElementById('counter');
+button.onclick=function(){
+    
+   //create a request object
+   
+   var request=XMLHttpRequest();
+   
+ 
+    request.onreadystatechange=function(){
+        if(request.readyStat===XMLHTTPRequest.Done)
+        {
+            //take some action
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+                
+            }
+            
+        }
+    };
+    // make a request
+    request.open('GET', 'http://tushargoyal2597.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
 
-  var button = document.getElementById('counter');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   //changing valvue by simply clicking button.
+  /*var button = document.getElementById('counter');
     var counter=0;
     button.onclick=function(){
-                                 //make a rseponse to the counter variable
-                          //captue tghe rsesponse and store it in variable
-                                     //render the variable to the correct span.
          counter = counter+1;
         var span=document.getElementById('count');
         span.innerHTML=counter.toString();                                                                          
-    };
+    };*/
         
 
 
 
- //  var element= document.getElementById("maintext");// changing the tewxt using javascript.
-  //  element.innerHTML="NEW VALUES SET USING JAVASCRIPT";
+ /*  var element= document.getElementById("maintext");// changing the tewxt using javascript.
+    element.innerHTML="NEW VALUES SET USING JAVASCRIPT";
+    */
 
 
 
 
-//making the image move
-//var t=document.getElementById('image');
-//t.onclick=function(){
-    //image.style.marginLeft='100px';
-//}
+/*making the image move
+var t=document.getElementById('image');
+t.onclick=function(){
+    image.style.marginLeft='100px';
+}*/
 
 
 
-                              // code to move image continuously on click
+                              /* code to move image continuously on click
                                     //  var img= document.getElementById('image');
                             //                                    var marginLeft=0;
                                 //function moveRight(){
@@ -39,9 +79,9 @@ console.log('Loaded!');
                      //img.onclick=function(){
                             // var interval=setInterval(moveRight,100)  ;
                                //                              };
+                              */
     
-    
-                                                        //counter code.
+                                                    
   
         
         
